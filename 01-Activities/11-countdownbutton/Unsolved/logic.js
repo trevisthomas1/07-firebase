6a0d74fc51f89b4000a9a7994c74cf2032ab5361
Trevis Thomas <trevislynnthomas@gmail.com>
2018-05-24 21:25:51 -0600

    Firebase Update

--- 01-Activities/11-countdownbutton/Unsolved/logic.js
+++ 01-Activities/11-countdownbutton/Unsolved/logic.js
@@ -2,15 +2,25 @@
 // Make sure that your configuration matches your firebase script version
 // (Ex. 3.0 != 3.7.1)
 
+var config = {
+  apiKey: "AIzaSyCoyo6in4ZS92aZF67Yg_kPQlEAU-D8W24",
+  authDomain: "fir-practice-209d5.firebaseapp.com",
+  databaseURL: "https://fir-practice-209d5.firebaseio.com",
+  projectId: "fir-practice-209d5",
+  storageBucket: "fir-practice-209d5.appspot.com",
+  messagingSenderId: "726745729586"
+};
+firebase.initializeApp(config);
+
 // Create a variable to reference the database
 // var database = ...
-
+var database = firebase.database();
 
 // Use the below initialValue
 var initialValue = 100;
 
 // Use the below variable clickCounter to keep track of the clicks.
-var clickCounter = initialValue;
+var count = initialValue;
 
 // --------------------------------------------------------------
 
@@ -18,6 +28,7 @@ var clickCounter = initialValue;
 // This callback keeps the page updated when a value changes in firebase.
 // HINT: Assuming 'database' is our database variable, use...
 // database.ref().on("value", function(snapshot)) {}
+database.ref().on("value",function(snapshot){
 
 // We are now inside our .on function...
 
@@ -28,12 +39,18 @@ var clickCounter = initialValue;
 // Change the value of our clickCounter to match the value in the database
 // ___________ = snapshot.val().______________________
 
+  count = snapshot.val().clicks;
+
 // Console Log the value of the clickCounter
 
 // Change the HTML using jQuery to reflect the updated clickCounter value
 
+  $("#click-value").html(count);
+
 // Then include Firebase error logging
 // HINT: }, function(errorObject)
+
+})
 
 // --------------------------------------------------------------
 
@@ -41,22 +58,24 @@ var clickCounter = initialValue;
 $("#click-button").on("click", function() {
 
   // Reduce the clickCounter by 1
-  clickCounter--;
+  count--;
 
   // Alert User and reset the counter
-  if (clickCounter === 0) {
+  if (count === 0) {
 
     alert("Phew! You made it! That sure was a lot of clicking.");
 
-    clickCounter = initialValue;
+    count = initialValue;
 
   }
 
   // Save new value to Firebase
-
+  firebase.database().ref().set({
+    clicks: count
+  });
 
   // Log the value of clickCounter
-  console.log(clickCounter);
+  console.log(count);
 
 });
 
@@ -64,16 +83,18 @@ $("#click-button").on("click", function() {
 $("#restart-button").on("click", function() {
 
   // Set the clickCounter back to initialValue
-  clickCounter = initialValue;
+  count = initialValue;
 
   // Save new value to Firebase
-
+  firebase.database().ref().set({
+    clicks: count
+  });
 
   // Log the value of clickCounter
-  console.log(clickCounter);
+  console.log(count);
 
   // Change the HTML Values
-  $("#click-value").text(clickCounter);
+  $("#click-value").text(count);
 
 
 });
